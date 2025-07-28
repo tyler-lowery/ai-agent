a20b67b4c7ead5024bae3791593df01dd4765d2d
tyler-lowery <tylerlowery.dev@gmail.com>
2025-07-27 20:58:12 -0500

    Added ability to write to files.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def test_get_files_info():
@@ -31,6 +32,18 @@ def test_get_file_content():
         print(get_file_content(*args))
 
 
+def test_write_file():
+    test_cases = [
+        ["calculator", "lorem.txt", "wait, this isn't lorem ipsum"],
+        ["calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"],
+        ["calculator", "/tmp/temp.txt", "this should not be allowed"],
+    ]
+    for args in test_cases:
+        print(f"Writing to file '{args[1]}':")
+        print(write_file(*args))
+
+
 if __name__ == "__main__":
     #test_get_files_info()
-    test_get_file_content()
+    #test_get_file_content()
+    test_write_file()
