7936c51300b44a8bebfa4253278504594d9bc3e2
tyler-lowery <tylerlowery.dev@gmail.com>
2025-07-27 20:50:02 -0500

    Added ability to read file contents.

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test_get_files_info():
@@ -8,16 +9,28 @@ def test_get_files_info():
         ["calculator", "/bin"],
         ["calculator", "../"],
     ]
-    
     for args in test_cases:
         working_directory = args[0]
         directory = args[1]
         if directory == ".":
             print("Result for current directory:")
         else:
-            print("Result for '{directory}' directory:")
+            print(f"Result for '{directory}' directory:")
         print(get_files_info(*args))
 
 
+def test_get_file_content():
+    test_cases = [
+        ["calculator", "main.py"],
+        ["calculator", "pkg/calculator.py"],
+        ["calculator", "/bin/cat"],
+        ["calculator", "pkg/does_not_exist.py"],
+    ]
+    for args in test_cases:
+        print(f"Contents of '{args[1]}':")
+        print(get_file_content(*args))
+
+
 if __name__ == "__main__":
-    test_get_files_info()
+    #test_get_files_info()
+    test_get_file_content()
