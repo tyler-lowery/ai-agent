9ef84d908238e61ea5e441447116d56b4293f904
tyler-lowery <tylerlowery.dev@gmail.com>
2025-07-27 20:24:53 -0500

    Store prompts and responses in a list for reference.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
@@ -15,17 +16,24 @@ def main():
         print('\nUsage: python main.py "your prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    user_prompt = " ".join(args)
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=user_prompt,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
