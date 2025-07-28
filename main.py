2c7b1ec233570e2950001a4979f0e33f213ef6b8
tyler-lowery <tylerlowery.dev@gmail.com>
2025-07-27 20:18:48 -0500

    Added connection to Google's Gemini API

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
